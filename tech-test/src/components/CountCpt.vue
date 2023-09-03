25e9135a5ab80b3311a714821e0336fc27aa491d
NCUCYY <2115895728@qq.com>
2023-09-03 20:00:49 +0800

    求和案例的vuex版本完成[补充store/index.js文件的上传]

---
+++ tech-test/src/components/CountCpt.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>当前求和为：{{ $store.state.sum }}</h1>
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">当前求和为奇数再加</button>
+    <button @click="incrementWait">等一等再加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountCpt',
+  data() {
+    return {
+      n: 1, //用户选择的数字
+    }
+  },
+  methods: {
+    increment() {
+      // 直接调mutations中的函数，操作数据
+      this.$store.commit('ADD', this.n)
+    },
+    decrement() {
+      // 直接调mutations中的函数，操作数据
+      this.$store.commit('REDUCE', this.n)
+    },
+    incrementOdd() {
+      // 先调actions中的函数，完成某些业务逻辑；再调mutations中的函数，操作数据
+      this.$store.dispatch('addOdd', this.n)
+    },
+    incrementWait() {
+      // 先调actions中的函数，完成某些业务逻辑；再调mutations中的函数，操作数据
+      this.$store.dispatch('addWait', this.n)
+    },
+  },
+}
+</script>
+
+<style lang="css">
+button {
+  margin-left: 5px;
+}
+</style>
---
+++ tech-test/src/store/index.js
@@ -0,0 +1,50 @@
+// 该文件用于创建Vuex中最为核心的store
+import Vue from 'vue'
+// 引入Vuex
+import Vuex from 'vuex'
+// 应用Vuex插件
+Vue.use(Vuex)
+
+// actions——用于响应组件中的动作
+const actions = {
+    // add(context, value) {
+    //     // context中有state
+    //     context.commit('ADD', value)
+    // },
+    // reduce(context, value) {
+    //     context.commit('REDUCE', value)
+    // },
+    addOdd(context, value) {
+        // 写业务逻辑
+        // context中有state
+        if (context.state.sum % 2 != 0) {
+            context.commit('ADD', value)
+        }
+    },
+    addWait(context, value) {
+        // 写业务逻辑
+        setTimeout(() => {
+            context.commit('ADD', value)
+        }, 500)
+    }
+}
+// mutations——用于操作数据（state）
+const mutations = {
+    ADD(state, value) {
+        // 真正修改数据
+        state.sum += value
+    },
+    REDUCE(state, value) {
+        // 真正修改数据
+        state.sum -= value
+    },
+}
+// state——用于存储数据
+const state = {
+    sum: 0
+}
+
+//创建并暴露store
+export default new Vuex.Store({
+    actions, mutations, state
+})
