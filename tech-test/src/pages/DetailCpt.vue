3146d1cde152f6357dc801ea6749a0f394e4c77f
NCUCYY <2115895728@qq.com>
2023-09-09 17:45:05 +0800

    路由配置文件的props配置项的使用

--- tech-test/src/pages/DetailCpt.vue
+++ tech-test/src/pages/DetailCpt.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
         <ul>
-            <li>消息编号:{{ $route.params.id }}</li>
-            <li>消息标题:{{ $route.params.title }}</li>
+            <li>消息编号:{{ id }}</li>
+            <li>消息标题:{{ title }}</li>
         </ul>
     </div>
 </template>
 
 <script>
 export default {
-    name: "DetailCpt"
+    name: "DetailCpt",
+    // 添加props配置项
+    props: ['id', 'title']
 }
 </script>
 
--- tech-test/src/router/index.js
+++ tech-test/src/router/index.js
@@ -27,7 +27,29 @@ export default new VueRouter({
                         {
                             name: 'detail',
                             path: 'detail/:id/:title', // 使用占位符生声明接收params参数
-                            component: DetailCpt
+                            component: DetailCpt,
+                            // 1）对象形式(一般不用；只能是写死的数据)
+                            // props:{
+                            //     id:'001',
+                            //     title:'message1'
+                            // }
+                            // 2）bool形式(只能出传递params中的参数)
+                            // props: true
+                            // 3）function形式(推荐；$route作为函数参数，返回值决定传递什么参数)
+                            // props($route) {
+                            //     // 由return返回值决定传递什么参数
+                            //     return {
+                            //         id: $route.params.id,
+                            //         title: $route.params.title
+                            //     }
+                            // }
+                            // 简化写法（解构赋值）
+                            props({ params }) {
+                                return {
+                                    id: params.id,
+                                    title: params.title
+                                }
+                            }
                         }
                     ]
                 }
