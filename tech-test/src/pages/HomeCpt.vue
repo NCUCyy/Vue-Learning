445a83fc9f7975d517077fe8e0d7d5895164f382
NCUCYY <2115895728@qq.com>
2023-09-09 15:32:53 +0800

    多级路由

--- tech-test/src/pages/HomeCpt.vue
+++ tech-test/src/pages/HomeCpt.vue
@@ -1,5 +1,18 @@
 <template>
-    <h2>我是Home的内容</h2>
+    <div>
+        <h2>我是Home的内容</h2>
+        <!-- 切换路由 -->
+        <ul class="nav nav-tabs">
+            <li>
+                <router-link class="list-group-item" active-class="active" to="/home/message">message</router-link>
+            </li>
+            <li>
+                <router-link class="list-group-item" active-class="active" to="/home/news">news</router-link>
+            </li>
+        </ul>
+        <!-- 展示二级路由 -->
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
--- tech-test/src/router/index.js
+++ tech-test/src/router/index.js
@@ -3,12 +3,26 @@ import VueRouter from 'vue-router'
 // 引入组件
 import HomeCpt from '../pages/HomeCpt'
 import AboutCpt from '../pages/AboutCpt'
+import NewsCpt from '../pages/NewsCpt'
+import MessageCpt from '../pages/MessageCpt'
 
 export default new VueRouter({
     routes: [
+        // 配置一级路由
         {
             path: '/home',
-            component: HomeCpt
+            component: HomeCpt,
+            // 配置二级路由(/home的子路由)
+            children: [
+                {
+                    path: 'news',
+                    component: NewsCpt
+                },
+                {
+                    path: 'message',
+                    component: MessageCpt
+                }
+            ]
         },
         {
             path: '/about',
