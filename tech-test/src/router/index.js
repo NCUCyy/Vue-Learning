091f743ae3774b500a527e4ccb56cb4f16f58d73
NCUCYY <2115895728@qq.com>
2023-09-09 22:19:10 +0800

    全局后置路由守卫---案例:document.title

--- tech-test/src/router/index.js
+++ tech-test/src/router/index.js
@@ -15,25 +15,27 @@ const router = new VueRouter({
             name: 'home',
             path: '/home',
             component: HomeCpt,
+            meta: { title: '主页' },
             // 配置二级路由(/home的子路由)
             children: [
                 {
                     name: 'news',
                     path: 'news',
                     component: NewsCpt,
-                    meta: { isAuth: true }
+                    meta: { isAuth: true, title: '新闻' }
                 },
                 {
                     name: 'message',
                     path: 'message',
                     component: MessageCpt,
-                    meta: { isAuth: true },
+                    meta: { isAuth: true, title: '消息' },
                     // 配置三级路由(/message的子路由)
                     children: [
                         {
                             name: 'detail',
                             path: 'detail/:id/:title', // 使用占位符生声明接收params参数
                             component: DetailCpt,
+                            meta: { title: '详情' },
                             // 简化写法（解构赋值）
                             props({ params }) {
                                 return {
@@ -49,7 +51,8 @@ const router = new VueRouter({
         {
             name: 'about',
             path: '/about',
-            component: AboutCpt
+            component: AboutCpt,
+            meta: { title: '关于' }
         }
     ]
 })
@@ -68,5 +71,9 @@ router.beforeEach((to, from, next) => {
         // 只有执行了next()，才会往后走
         next()
     }
+})
+// 全局后置路由守卫————①初始化时被调用；②每一次路由切换之后被调用
+router.afterEach((to, from) => {
+    document.title = to.meta.title
 })
 export default router
