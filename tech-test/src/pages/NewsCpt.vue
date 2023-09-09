a6cfdbe5832cee383504c0b75485e9d3aa809bb4
NCUCYY <2115895728@qq.com>
2023-09-09 21:18:49 +0800

    activated和deactivated两个生命周期

--- tech-test/src/pages/NewsCpt.vue
+++ tech-test/src/pages/NewsCpt.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <ul>
+            <li :style="{ opacity }">欢迎学习Vue</li>
             <li>
                 news001
                 <input type="text" />
@@ -22,7 +23,19 @@ export default {
     name: "NewsCpt",
     beforeDestroy(){
         console.log('NewsCpt组件被销毁！')
-    }
+    },
+    activated() {
+        console.log('News组件被激活了')
+        this.timer = setInterval(() => {
+            console.log('@')
+            this.opacity -= 0.01
+            if (this.opacity <= 0) this.opacity = 1
+        }, 16)
+    },
+    deactivated() {
+        console.log('News组件失活了')
+        clearInterval(this.timer)
+    },
 }
 </script>
 
